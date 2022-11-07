7fdc0f5f43d260c937c2052904347ab64004c23d
Mansur <mahcypc@mail.ru>
2022-11-07 23:24:07 +0300

    Cleaned project

--- app/src/main/java/com/example/neco/MainActivity.kt
+++ app/src/main/java/com/example/neco/MainActivity.kt
@@ -12,40 +12,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
-        val infoMsg = getString(R.string.infoMsg)
-
-        bindingClass.bt.setOnClickListener {
-            val pass = bindingClass.edPass.text.toString()
-            bindingClass.tvResult.visibility = View.VISIBLE
-            when(bindingClass.edText.text.toString().uppercase()){
-
-                Constance.SECURITY -> {if(pass == Constance.SECURITY_PASSWORD){
-                    bindingClass.tvResult.text = Constance.SECURITY_SALARY
-                    bindingClass.imPhoto.setImageResource(R.drawable.security)
-                }
-                    else {bindingClass.tvResult.text = infoMsg
-                    bindingClass.imPhoto.setImageResource(R.drawable.stop)}
-                }
-
-                Constance.INGENEER -> {if(pass == Constance.INGENEER_PASSWORD){
-                    bindingClass.tvResult.text = Constance.INGENEER_SALARY
-                    bindingClass.imPhoto.setImageResource(R.drawable.engineer)
-                }
-                else {bindingClass.tvResult.text = infoMsg
-                    bindingClass.imPhoto.setImageResource(R.drawable.stop)}
-                }
-
-                Constance.DIRECTOR -> {if(pass == Constance.DIRECTOR_PASSWORD){
-                    bindingClass.tvResult.text = Constance.DIRECTOR_SALARY
-                    bindingClass.imPhoto.setImageResource(R.drawable.director)
-                }
-                else {bindingClass.tvResult.text = infoMsg
-                    bindingClass.imPhoto.setImageResource(R.drawable.stop)}
-                }
-
-                else -> {bindingClass.tvResult.text = getString(R.string.elseMsg)
-                    bindingClass.imPhoto.setImageResource(R.drawable.stop)}
-            }
         }
-    }
+
 }
