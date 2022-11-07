d771c5bc3d0c4a74c9d92184d29f5254a11e7dfa
Mansur <mahcypc@mail.ru>
2022-11-08 00:50:53 +0300

    Created intents

--- app/src/main/java/com/example/neco/MainActivity.kt
+++ app/src/main/java/com/example/neco/MainActivity.kt
@@ -1,22 +1,31 @@
 package com.example.neco
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import com.example.neco.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var bindingClass : ActivityMainBinding
+    private lateinit var bindingClass : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
 
-        bindingClass.btGoToSecond.setOnClickListener(){
+        bindingClass.btGoToSecond.setOnClickListener{
+            val i = Intent(this, SecondActivity::class.java)
+            i.putExtra("question", "Как тебя зовут?")
+            startActivityForResult(i, 100)
+        }
+        }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 100 && resultCode == RESULT_OK && data != null){
+            bindingClass.tvAnswer.text = data.getStringExtra("answer")
         }
-        }
+    }
 
 
 }
--- app/src/main/java/com/example/neco/SecondActivity.kt
+++ app/src/main/java/com/example/neco/SecondActivity.kt
@@ -12,5 +12,14 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         bindingClass = ActivitySecondBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
+
+        val message = intent.getStringExtra("question")
+        bindingClass.tvMessage.text = message
+
+        bindingClass.btGoToMain.setOnClickListener(){
+            intent.putExtra("answer", bindingClass.edAnswer.text.toString())
+            setResult(RESULT_OK, intent)
+            finish()
+        }
     }
 }
