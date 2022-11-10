703caa41de4c94b49e65a4752132d7f5bf7a98fd
Mansur <mahcypc@mail.ru>
2022-11-11 00:02:34 +0300

    Buttons invisible

--- app/src/main/java/com/example/neco/MainActivity.kt
+++ app/src/main/java/com/example/neco/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.neco
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.neco.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -23,6 +24,9 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if(requestCode == 100 && resultCode == RESULT_OK && data != null){
+            bindingClass.signUp.visibility = View.INVISIBLE
+            bindingClass.signIn.visibility = View.INVISIBLE
+            bindingClass.quit.visibility = View.VISIBLE
             bindingClass.tvMainInfo.text = data.getStringExtra("answer")
         }
     }
